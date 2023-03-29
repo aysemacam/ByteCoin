5cb3d634967da242f5af116b899ef7464398fa13
aysemacam <aysema584@gmail.com>
2023-03-30 00:36:47 +0300

    Initial Commit

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,64 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+protocol CoinManagerDelegete {
+    
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+        
+}
+
+struct CoinManager {
+    var delegete: CoinManagerDelegete?
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "A54D11C0-0DDB-4C8E-9A5D-25799BBE95F2"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    delegete?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                  if let bitcoinPrice = parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegete?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+                
+            }
+            task.resume()
+            
+        }
+        func parseJSON(_ data: Data) -> Double? {
+            let decoder = JSONDecoder()
+            do {
+                let decodedData = try decoder.decode(CoinData.self, from: data)
+                let lastPrice = decodedData.rate
+                return lastPrice
+                
+            } catch {
+                print(error)
+                return nil
+                
+            }
+            
+        }
+    }
+}
