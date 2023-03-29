d03699e7faa5dc7299aaf0cb3a3cae8cc43a041f
aysemacam <aysema584@gmail.com>
2023-03-30 00:36:47 +0300

    Initial Commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Akçay Holding on 20.03.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Decodable {
+    let rate: Double
+}
+
+
